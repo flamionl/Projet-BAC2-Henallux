90a96fdd0d10e76f9f2b71e70b38f5a9f8f13635
Louis <louis.flamion@gmail.com>
2021-12-02 10:16:24 +0100

    generate_key implementation

--- ransom.c
+++ ransom.c
@@ -4,6 +4,7 @@
 #include <sys/socket.h>
 #include <unistd.h> 
 #include <arpa/inet.h>
+#include <openssl/rand.h>
 
 
 
@@ -20,6 +21,21 @@ int send_key(char *pKey, char *pIv);
 int main (int argc, char * argv[])
 {
 	printf("Bon travail!\n");
+	unsigned char key[33];
+    int sizeKey = 33;
+    unsigned char iv[17];
+    int sizeIv = 17;
+    char pKey[65];
+    char pIv[33];
+	int status = generate_key(key, sizeKey, iv, sizeIv, pKey, pIv);
+	printf("%d\n", status);
+	printf("%s", key);
 }
 
-
+int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv,char *pKey, char *pIv)
+{
+	RAND_priv_bytes(key, sizeKey);
+	RAND_priv_bytes(iv, sizeIv);
+	bytes_to_hexa(key, pKey,sizeKey );
+	bytes_to_hexa(iv, pIv, sizeKey);
+}
